6a78d43d534b66f7f916e3b2f751eca7ad6548bf
Gabriel Trabasso <gabr.j.t@gmail.com>
2022-01-18 11:29:15 -0300

    Setup paginated text

---
+++ Assets/_Project/PaginatedText/PaginatedTextContainer.cs
@@ -0,0 +1,48 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PaginatedTextContainer : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] PaginatedText _paginatedText;
+    [SerializeField] GameObject _panel;
+    [SerializeField] TMP_Text _text;
+    [SerializeField] UnityEvent _paginationFinished;
+
+    void OnEnable()
+    {
+        _text.text = string.Empty;
+
+        Paginate();
+    }
+
+    void OnValidate()
+    {
+        Assert.IsNotNull(_paginatedText);
+        Assert.IsNotNull(_panel);
+        Assert.IsNotNull(_text);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Paginate();
+    }
+
+    void Paginate()
+    {
+        var result = _paginatedText.TryGetNextPage(out var page);
+
+        _panel.gameObject.SetActive(result);
+
+        if (result)
+        {
+            _text.text = page;
+
+            return;
+        }
+
+        _paginationFinished.Invoke();
+    }
+}
