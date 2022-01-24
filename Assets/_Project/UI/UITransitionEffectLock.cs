e8fabf67401382f7efe962804828ee43a48b904c
Gabriel Trabasso <gabr.j.t@gmail.com>
2022-01-23 21:03:16 -0300

    Wait for text to finish printing to show button after selection

--- Assets/_Project/UI/UITransitionEffectLock.cs
+++ Assets/_Project/UI/UITransitionEffectLock.cs
@@ -1,21 +1,52 @@
 using Coffee.UIEffects;
+using Cysharp.Threading.Tasks;
 using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(UITransitionEffect))]
+[RequireComponent(typeof(UITransitionEffect), typeof(Image))]
 public class UITransitionEffectLock : MonoBehaviour
 {
     [SerializeField] [Required] UITransitionEffect _uiTransitionEffect;
-    [ShowNonSerializedField] bool _locked;
+    [SerializeField] [Required] Image _image;
+    [SerializeField] bool _locked = true;
+    readonly CancellationTokenContainer _cancellationToken = new();
+
+    void OnDisable()
+    {
+        _cancellationToken.Cancel();
+    }
 
     void OnValidate()
     {
         _uiTransitionEffect = GetComponent<UITransitionEffect>();
+        _image = GetComponent<Image>();
     }
 
     public void SetLocked(bool locked)
     {
         _locked = locked;
+    }
+
+    public bool IsUnlocked()
+    {
+        return !_locked;
+    }
+
+    public async void WaitUnlockAndShow()
+    {
+        _cancellationToken.Cancel();
+
+        var cancelled = await UniTask.WaitUntil(IsUnlocked, PlayerLoopTiming.Update, _cancellationToken.CancellationToken).SuppressCancellationThrow();
+
+        if (cancelled)
+        {
+            return;
+        }
+
+        _image.enabled = true;
+
+        Show();
     }
 
     public void Show()
