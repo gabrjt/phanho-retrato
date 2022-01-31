676a4caa7a3552ca3749e3588df2c598cf3017ff
Gabriel Trabasso <gabr.j.t@gmail.com>
2022-01-31 11:23:37 -0300

    Update packages and add controls

---
+++ Assets/_Project/Controls/Controls.cs
@@ -0,0 +1,29 @@
+#if UNITY_EDITOR || UNITY_STANDALONE
+#define ENABLE_CONTROLS
+#endif
+
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Controls : MonoBehaviour
+{
+    [Conditional("ENABLE_CONTROLS")]
+    void Update()
+    {
+        Assert.IsTrue(Application.isPlaying);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
